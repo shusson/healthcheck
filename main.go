e5eddaf1ca9597e5f99f1a9112d1ed39592b1466
Shane Husson <shane.a.husson@gmail.com>
2017-06-20 11:11:01 +1000

    add simple health check

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"bytes"
+	"errors"
 )
 
 func main() {
@@ -25,22 +27,47 @@ func main() {
 	}
 
 	r := mux.NewRouter().StrictSlash(true)
-	r.HandleFunc("/", Index(r))
+	r.HandleFunc("/", Index(r, url))
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
-func Index(router *mux.Router) http.HandlerFunc {
+func Index(router *mux.Router, url string) http.HandlerFunc {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Mapd healthcheck")
+		var jsonStr = []byte(`[1,"connect",1,0,{"1":{"str":""},"2":{"str":""},"3":{"str":""}}]`)
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+
+		if check(err, w) != nil {
+			return
+		}
+		req.Header.Set("Accept", "application/vnd.apache.thrift.json; charset=utf-8")
+		req.Header.Set("Content-Type", "application/vnd.apache.thrift.json; charset=UTF-8")
+		req.Header.Set("Content-Length", "88")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if check(err, w) != nil {
+			return
+		}
+		defer resp.Body.Close()
+
+		var status error
+		if resp.StatusCode != 200 {
+			status = errors.New(resp.Status)
+		}
+		if check(status, w) != nil {
+			return
+		}
+		w.Header().Set("Server", "Mapd healthcheck")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
 	}
 	return http.HandlerFunc(fn)
 }
 
-func check(err error) {
-	if err == nil {
-		return
+func check(err error, w http.ResponseWriter) error {
+	if err != nil {
+		http.Error(w, err.Error(), 500)
 	}
-	panic(err)
+	return err
 }
-
